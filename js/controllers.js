dcdd4641b28c8de7c75e7218bf14004b5ba06a3a
IrvingWang <irving@mail.searching-service.com>
2015-08-12 14:56:48 +0800

    update

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,11 @@
-var map=null;
 angular.module('myWeddingInfo.controllers', [])
     .controller('mainCtrl', function ($scope, $ionicModal, $location, $ionicPopup, $ionicLoading, $ionicScrollDelegate, Common, DataParse) {
         $scope.weddingInfo = {};
         $('#twzipcode').twzipcode();
         $scope.showMask = function () {
-            $ionicLoading.show({template: '傳送中,請稍後...'});
+            $ionicLoading.show({
+                template: '傳送中,請稍後...'
+            });
         };
         $scope.hideMask = function () {
             $ionicLoading.hide();
@@ -24,16 +25,13 @@ angular.module('myWeddingInfo.controllers', [])
                                 if (!$scope.weddingInfo.AttendanceNum) {
                                     $('#attendanceNum').attr('placeholder', '請輸入人數!');
                                     e.preventDefault();
-                                }
-                                else {
+                                } else {
                                     var attendanceNum = $scope.weddingInfo.AttendanceNum;
                                     if (attendanceNum < 3) {
                                         $scope.weddingInfo.AttendanceNum = 3;
-                                    }
-                                    else if (attendanceNum > 5) {
+                                    } else if (attendanceNum > 5) {
                                         $scope.weddingInfo.AttendanceNum = 5;
-                                    }
-                                    else {
+                                    } else {
                                         $scope.weddingInfo.AttendanceNum = parseInt($scope.weddingInfo.AttendanceNum);
                                     }
                                 }
@@ -60,8 +58,7 @@ angular.module('myWeddingInfo.controllers', [])
                     DataParse.Update(result, params, function () {
                         window.location = '/#/app/thanks';
                     });
-                }
-                else {
+                } else {
                     DataParse.Add(params, function () {
                         window.location = '/#/app/thanks';
                     });
@@ -73,59 +70,56 @@ angular.module('myWeddingInfo.controllers', [])
             DataParse.Query(params, callback);
         };
         $scope.ValidateData = function (data) {
-            console.log(data);
             if (!data.Name) {
                 Common.showAlert('提示', '名字還沒填唷!');
                 return false;
-            }
-            else if (!data.Phone) {
+            } else if (!data.Phone) {
                 Common.showAlert('提示', '手機還沒填唷!');
                 return false;
-            }
-            else if (!data.Phone.match(/^09[0-9]{8}$/)) {
+            } else if (!data.Phone.match(/^09[0-9]{8}$/)) {
                 Common.showAlert('提示', '手機格式有誤!', function () {
                     data.Phone = null;
                 });
 
                 return false;
-            }
-            else if (!data.Group) {
+            } else if (!data.Group) {
                 Common.showAlert('提示', '還沒選擇你是哪方人馬唷!');
                 return false;
-            }
-            else if (!data.Attendance) {
+            } else if (!data.Attendance) {
                 Common.showAlert('提示', '還沒選擇參加人數唷!');
                 return false;
-            }
-            else if (!$('#county').val() || !$('#district').val()) {
+            } else if (!$('#county').val() || !$('#district').val()) {
                 Common.showAlert('提示', '還沒選擇喜帖寄送縣市唷!');
                 return false;
-            }
-            else if (!data.Address) {
+            } else if (!data.Address) {
                 Common.showAlert('提示', '地址還沒填唷!');
                 return false;
             }
             return true;
         };
     })
-    .controller('loginCtrl', function ($scope, loginParse, $location, $localStorage, $ionicLoading, $window,Common) {
+    .controller('loginCtrl', function ($scope, loginParse, $location, $localStorage, $ionicLoading, $window, Common) {
         $scope.username = '';
         $scope.password = '';
         $scope.logon = function (username, password) {
             if (!username) {
                 Common.showAlert('提示', '請輸入帳號!');
                 return;
-            }
-            else if (!password) {
+            } else if (!password) {
                 Common.showAlert('提示', '請輸入密碼!');
                 return;
             }
-            $ionicLoading.show({template: '登入中,請稍後...'});
-            var object = {'username': username, 'password': password};
+            $ionicLoading.show({
+                template: '登入中,請稍後...'
+            });
+            var object = {
+                'username': username,
+                'password': password
+            };
             loginParse.Login(object).then(function (result) {
                 $ionicLoading.hide();
                 $localStorage.token = result.data.sessionToken;
-                $window.location.href= '#/backend/main';
+                $window.location.href = '#/backend/main';
             }, function (error) {
                 $ionicLoading.hide();
                 console.warn(error);
@@ -151,39 +145,42 @@ angular.module('myWeddingInfo.controllers', [])
             var callback = function (result) {
                 for (var i = 0; i < result.length; i++) {
                     var object = result[i];
-                    $scope.listAttend.push(
-                        {
-                            Name: object.get('Name'),
-                            Phone: object.get('Phone'),
-                            Group: object.get('Group'),
-                            Attendance: object.get('GrAttendanceoup'),
-                            Address: object.get('Address'),
-                            ThanksMemo: object.get('ThanksMemo')
-                        });
+                    $scope.listAttend.push({
+                        Name: object.get('Name'),
+                        Phone: object.get('Phone'),
+                        Group: object.get('Group'),
+                        Attendance: object.get('GrAttendanceoup'),
+                        Address: object.get('Address'),
+                        ThanksMemo: object.get('ThanksMemo')
+                    });
                 }
             };
             DataParse.QueryAll(callback);
 
-        }
-        else {
+        } else {
             $location.path('/login');
         }
         $scope.Logout = function () {
-            if($scope.loginout){
-                $scope.loginout=false;
+            if ($scope.loginout) {
+                $scope.loginout = false;
             }
             delete $localStorage.token;
             //$window.location.reload('true');
-            $window.location.href='#/login';
+            $window.location.href = '#/login';
         };
     })
-.controller('thanksCtrl', function ($scope, MAP) {
-    $scope.poi={
-        "name":"基隆港海產樓",
-        "address":"基隆市信義區信二路181號",
-        "lating":[25.1295278,121.7508176]
-    };
-    map = MAP.initialize('map'); 
-    var marker = {lat: ($scope.poi.latlng)[0], lng: ($scope.poi.latlng)[1], title: $scope.poi.name};
-    MAP.setLocation(map, marker);
-});
+    .controller('thanksCtrl', function ($scope, MAP) {
+        $scope.poi = {
+            "name": "基隆港海產樓",
+            "address": "基隆市信義區信二路181號",
+            "lat": 25.1295278,
+            "lng": 121.7508176
+        };
+        map = MAP.initialize('map');
+        var marker = {
+            lat: $scope.poi.lat,
+            lng: $scope.poi.lng,
+            title: $scope.poi.name
+        };
+        MAP.setLocation(map, marker);
+    });
